a06ee68853e1e0796a3b4f0ff0c4b385f19f872b
Jason Lefever <jlefever@ycp.edu>
2019-07-17 21:54:47 -0400

    Add a AstGenerator tool

---
+++ Lox.Tools/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Lox.Tools
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string directory = "../../../../Lox/";
+            const string @namespace = "Lox";
+            const string baseName = "Expr";
+
+            var descriptions = new[]
+            {
+                "BinaryExpr   : Expr Left, Token Op, Expr Right",
+                "GroupingExpr : Expr Expression",
+                "LiteralExpr  : object Value",
+                "UnaryExpr    : Token Op, Expr Right"
+            };
+
+            var generator = new AstGenerator(descriptions, baseName, @namespace);
+            var ast = generator.DefineAst();
+
+            var path = Path.Combine(directory, baseName + ".cs");
+            using var writer = new StreamWriter(path);
+            writer.WriteLine(ast);
+        }
+    }
+}
